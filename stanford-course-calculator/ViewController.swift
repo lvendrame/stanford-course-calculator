e6bc9bf0e318fd74cd52d689f586f56e9d7af61b
Luís Vendrame <lvendrame@projeto12-mac-mini-2.local>
2017-02-04 13:31:36 -0200

    Initial Commit

---
+++ stanford-course-calculator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  stanford-course-calculator
+//
+//  Created by Luís Vendrame on 04/02/17.
+//  Copyright © 2017 Luís Vendrame. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
